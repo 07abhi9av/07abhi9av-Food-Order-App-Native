c0105ed62650cdff85f24ff9fa19441dfe4583aa
Abhinav Aryan <coolpadrocx@gmail.com>
2023-12-21 23:30:52 +0530

    Initial commit

---
+++ client/App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Navigation from './navigation';
+import { Provider } from 'react-redux'
+import { store } from './store';
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <Navigation/>
+    </Provider>
+  );
+}
---
+++ client/components/categories.js
@@ -0,0 +1,48 @@
+import { View, Text, TouchableOpacity, Image, ScrollView } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { categories } from '../constants';
+import { getCategories } from '../api';
+import { urlFor } from '../sanity';
+
+export default function Categories() {
+
+    const [activeCategory, setActiveCategory] = useState(null);
+    let [categories, setCategories] = useState([]);
+
+    useEffect(()=>{
+        getCategories().then(data=>{
+            setCategories(data);
+        })
+    })
+    
+  return (
+    <View className="mt-5">
+        <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            className="overflow-visible"
+            contentContainerStyle={{paddingHorizontal: 15}}
+        >
+            {
+                categories.map((category, index)=>{
+                    let isActive = category._id==activeCategory;
+                    let btnClass = isActive? ' bg-gray-600' : ' bg-gray-200';
+                    let textClass = isActive? ' font-semibold' : ' text-gray-500';
+                    return(
+                        <View key={index} className="flex justify-center items-center mr-6">
+                            <TouchableOpacity 
+                                onPress={()=> setActiveCategory(category._id)} 
+                                className={"p-1 rounded-full shadow"+ btnClass}
+                            >
+                                <Image style={{width: 45, height: 45}} source={{uri: urlFor(category.image).url()}}/>
+                            </TouchableOpacity>
+                            <Text className={"text-sm "+textClass}>{category.name}</Text>
+                        </View>
+                    )
+                })
+            }
+
+        </ScrollView>
+    </View>
+  )
+}
